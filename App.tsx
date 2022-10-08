ab8cd8624abafc8d8cf606d4378d8a8b676458f0
Koralesky <dominik.koralewski@gmail.com>
2022-10-08 13:55:51 +0200

    Navigation added

--- App.tsx
+++ App.tsx
@@ -2,14 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import * as eva from "@eva-design/eva";
 import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import ChooseProfile from "./src/Scenes/ChooseProfile/ChooseProfile";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </Layout>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={ChooseProfile}
+            options={{ title: "Welcome" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ApplicationProvider>
   );
 }
---
+++ src/Scenes/ChooseProfile/ChooseProfile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Layout, Text } from "@ui-kitten/components";
+
+const ChooseProfile = () => {
+  return (
+    <Layout>
+      <Text>ChooseProfile</Text>
+    </Layout>
+  );
+};
+
+export default ChooseProfile;
